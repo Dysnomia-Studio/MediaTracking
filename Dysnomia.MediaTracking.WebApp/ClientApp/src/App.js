1e3acd290ea33a50dba5f7f29568bb70fbce35fd
Elanis <elanis@hotmail.com>
2022-05-28 20:20:16 +0200

    Setup eslint

--- Dysnomia.MediaTracking.WebApp/ClientApp/src/App.js
+++ Dysnomia.MediaTracking.WebApp/ClientApp/src/App.js
@@ -5,18 +5,18 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-import './custom.css'
+import './custom.css';
 
 export default class App extends Component {
-  static displayName = App.name;
+	static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route path='/counter' element={<Counter />} />
-        <Route path='/fetch-data' element={<FetchData />} />
-        <Route path='/' element={<Home />} />
-      </Layout>
-    );
-  }
+	render () {
+		return (
+			<Layout>
+				<Route path='/counter' element={<Counter />} />
+				<Route path='/fetch-data' element={<FetchData />} />
+				<Route path='/' element={<Home />} />
+			</Layout>
+		);
+	}
 }
--- Dysnomia.MediaTracking.WebApp/ClientApp/src/components/Layout.js
+++ Dysnomia.MediaTracking.WebApp/ClientApp/src/components/Layout.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 import {
-  Routes,
-} from "react-router-dom";
+	Routes,
+} from 'react-router-dom';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+	static displayName = Layout.name;
 
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          <Routes>
-          {this.props.children}
-          </Routes>
-        </Container>
-      </div>
-    );
-  }
+	render () {
+		return (
+			<div>
+				<NavMenu />
+				<Container>
+					<Routes>
+						{this.props.children}
+					</Routes>
+				</Container>
+			</div>
+		);
+	}
 }
